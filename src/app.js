6ccb96e8123ed32075f3adcaebf44a4f13ed91be
carlos-wylliam <carloswylliam023@gmail.com>
2025-08-21 21:26:54 -0300

    bendito cors

--- src/app.js
+++ src/app.js
@@ -4,8 +4,15 @@ const { router } = require("./routes");
 
 const app = express();
 
-// Habilita CORS para qualquer origem (pode restringir depois)
-app.use(cors());
+// Configuração do CORS
+const corsOptions = {
+  origin: ["https://acertvo-literario-backend.onrender.com"], // substitua pela URL do seu frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+// Habilita CORS com as opções
+app.use(cors(corsOptions));
 
 // Habilita JSON no body das requisições
 app.use(express.json());
